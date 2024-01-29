2571244200c592734465afcd60749e76a4c33e4d
veeraja <seventhhokagedattebayo@gmail.com>
2024-01-29 12:55:52 +0530

    Week3 part 1 Student class completed

--- src/Week3/Student.java
+++ src/Week3/Student.java
@@ -3,10 +3,16 @@ package Week3;
 public class Student {
     static String collegename="NIT ANP";
     int rollno;
+    String name;
 
+    Student(int roll, String n){
+        rollno=roll;
+        name=n;
+    }
     void display(){
         System.out.println("College name:"+collegename);
         System.out.println("Roll no:"+ rollno);
+        System.out.println("Name:"+name);
     }
 
     static void changeCollege(String clgname){
@@ -14,8 +20,7 @@ public class Student {
     }
 
     public static void main(String[] args){
-        Student ob = new Student();
-        ob.rollno=80;
+        Student ob = new Student(80, "Veeraja");
         ob.display();
         Student.changeCollege("NIT AP");
         ob.display();
